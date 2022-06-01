1f95fbec5fa1cb1e0e79e0c71ad767a00b2c9f87
rmntmo <romantimos@gmail.com>
2022-06-01 23:25:13 +0300

    make pretty

--- frontend/components/document/LibPanel.tsx
+++ frontend/components/document/LibPanel.tsx
@@ -1,105 +1,104 @@
-import styles from '../../styles/LibPanel.module.css'
-import React, {useState} from 'react';
-import {AtlasNode, ExpressionNode, TextNode, FileNode} from '../../utils/AtlasGraph';
+import styles from '../../styles/LibPanel.module.css';
+import React, { useState } from 'react';
+import { AtlasNode, ExpressionNode, TextNode, FileNode } from '../../utils/AtlasGraph';
 
 type Props = {
-    setDruggedNode: React.Dispatch<React.SetStateAction<AtlasNode | null>>;
-    libPanelStyleWrapper: string;
+	setDruggedNode: React.Dispatch<React.SetStateAction<AtlasNode | null>>;
+	libPanelStyleWrapper: string;
 };
 
+export default function LibPanel({ setDruggedNode, libPanelStyleWrapper }: Props): JSX.Element {
+	const onDragStart = (event: React.DragEvent<HTMLDivElement>, node: AtlasNode) => {
+		setDruggedNode(node);
+		event.dataTransfer.effectAllowed = 'move';
+	};
 
-export default function LibPanel({setDruggedNode, libPanelStyleWrapper}: Props): JSX.Element {
-    const onDragStart = (event: React.DragEvent<HTMLDivElement>, node: AtlasNode) => {
-        setDruggedNode(node);
-        event.dataTransfer.effectAllowed = 'move';
-    };
+	const nodesOptions = {
+		ExpressionNode: () =>
+			new ExpressionNode(
+				new AtlasNode(ExpressionNode.structType, '', 'pkg', [0, 0], true),
+				'2 + 3',
+				'5',
+			),
+		TextNode: () =>
+			new TextNode(
+				new AtlasNode(TextNode.structType, 'name1', 'pkg', [0, 0], true),
+				'1, 2, 3',
+			),
+		FileNode: () =>
+			new FileNode(new AtlasNode(FileNode.structType, 'name1', 'pkg', [0, 0], true), '', ''),
+	};
 
-    const nodesOptions = {
-        ExpressionNode: () =>
-            new ExpressionNode(
-                new AtlasNode(ExpressionNode.structType, '', 'pkg', [0, 0], true),
-                '2 + 3',
-                '5',
-            ),
-        TextNode: () =>
-            new TextNode(
-                new AtlasNode(TextNode.structType, 'name1', 'pkg', [0, 0], true),
-                '1, 2, 3',
-            ),
-        FileNode: () =>
-            new FileNode(new AtlasNode(FileNode.structType, 'name1', 'pkg', [0, 0], true), '', ''),
-    };
+	const libElements = {
+		// Symbolic: ['Simplify', 'Equal'],
+		// Graphics: ['2D-plot', '3D-plot'],
+		// Engineering: ['PID Controller'],
+		// Import: ['JSON', 'CSV', 'XML', 'Form'],
+		// Physical: ['Custom Object'],
+		Basic: nodesOptions,
+		Symbolic: nodesOptions,
+		Graphics: nodesOptions,
+		Engineering: nodesOptions,
+		Import: nodesOptions,
+		Physical: nodesOptions,
+	};
 
+	const libCollapseStates = {
+		Basic: useState<string>(''),
+		Symbolic: useState<string>(styles.containerCollapse),
+		Graphics: useState<string>(styles.containerCollapse),
+		Engineering: useState<string>(styles.containerCollapse),
+		Import: useState<string>(styles.containerCollapse),
+		Physical: useState<string>(styles.containerCollapse),
+	};
 
-    const libElements = {
-        // Symbolic: ['Simplify', 'Equal'],
-        // Graphics: ['2D-plot', '3D-plot'],
-        // Engineering: ['PID Controller'],
-        // Import: ['JSON', 'CSV', 'XML', 'Form'],
-        // Physical: ['Custom Object'],
-        Basic: nodesOptions,
-        Symbolic: nodesOptions,
-        Graphics: nodesOptions,
-        Engineering: nodesOptions,
-        Import: nodesOptions,
-        Physical: nodesOptions,
-    };
+	const openOrCollapseLibSection = (idName: keyof typeof libCollapseStates): void => {
+		libCollapseStates[idName][1](
+			libCollapseStates[idName][0] === '' ? styles.containerCollapse : '',
+		);
+	};
 
-    const libCollapseStates = {
-        Basic: useState<string>(''),
-        Symbolic: useState<string>(styles.containerCollapse),
-        Graphics: useState<string>(styles.containerCollapse),
-        Engineering: useState<string>(styles.containerCollapse),
-        Import: useState<string>(styles.containerCollapse),
-        Physical: useState<string>(styles.containerCollapse),
-    };
+	function getLibElements(name: keyof typeof nodesOptions): JSX.Element {
+		return (
+			<div
+				key={name}
+				className={styles.elementSingle}
+				onDragStart={(event) => onDragStart(event, nodesOptions[name]())}
+				draggable
+			>
+				<span>{name.slice(0, 4)}</span>
+			</div>
+		);
+	}
 
-    const openOrCollapseLibSection = (idName: keyof typeof libCollapseStates): void => {
-        libCollapseStates[idName][1](
-            libCollapseStates[idName][0] === '' ? styles.containerCollapse : '',
-        );
-    };
+	function getLibSections(libName: keyof typeof libElements): JSX.Element {
+		return (
+			<div key={libName} className={styles.libWrapper}>
+				<div
+					id={libName}
+					className={styles.libSectionLabel}
+					onClick={(evt) => {
+						openOrCollapseLibSection(evt.currentTarget.id as keyof typeof libElements);
+					}}
+				>
+					<label>
+						{'>'} {libName}
+					</label>
+				</div>
+				<div className={`${styles.elementsContainer} ${libCollapseStates[libName][0]}`}>
+					{Object.keys(libElements[libName]).map((elem) =>
+						getLibElements(elem as keyof typeof nodesOptions),
+					)}
+				</div>
+			</div>
+		);
+	}
 
-    function getLibElements(name: keyof typeof nodesOptions): JSX.Element {
-        return (
-            <div
-                key={name}
-                className={styles.elementSingle}
-                onDragStart={(event) => onDragStart(event, nodesOptions[name]())}
-                draggable
-            >
-                <span>{name.slice(0, 4)}</span>
-            </div>
-        );
-    }
-
-
-    function getLibSections(libName: keyof typeof libElements): JSX.Element {
-        return (
-            <div key={libName} className={styles.libWrapper}>
-                <div
-                    id={libName}
-                    className={styles.libSectionLabel}
-                    onClick={(evt) => {
-                        openOrCollapseLibSection(evt.currentTarget.id as keyof typeof libElements);
-                    }}
-                >
-                    <label>
-                        {'>'} {libName}
-                    </label>
-                </div>
-                <div className={`${styles.elementsContainer} ${libCollapseStates[libName][0]}`}>
-                    {Object.keys(libElements[libName]).map((elem) => getLibElements(elem as keyof typeof nodesOptions))}
-                </div>
-            </div>
-        );
-    }
-
-    return (
-        <div className={`${libPanelStyleWrapper}`}>
-            {Object.keys(libElements).map((name) =>
-                getLibSections(name as keyof typeof libCollapseStates),
-            )}
-        </div>
-    );
+	return (
+		<div className={`${libPanelStyleWrapper}`}>
+			{Object.keys(libElements).map((name) =>
+				getLibSections(name as keyof typeof libCollapseStates),
+			)}
+		</div>
+	);
 }
--- frontend/pages/document.tsx
+++ frontend/pages/document.tsx
@@ -1,11 +1,11 @@
-import DnDFlow, {atlasGraph} from '../components/document/DnDFlow';
+import DnDFlow, { atlasGraph } from '../components/document/DnDFlow';
 import Head from 'next/head';
 import styles from '../styles/main.module.css';
 import btnStyles from '../styles/BtnStyle.module.css';
 import ElementsPanel from '../components/document/ElementsPanel';
 import PropsPanel from '../components/document/PropsPanel';
 import LibPanel from '../components/document/LibPanel';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import menuImg from '/img/icons/menu.png';
 import questionImg from '/img/icons/question.png';
@@ -13,142 +13,134 @@ import settingsImg from '/img/icons/settings.png';
 import exportImg from '/img/icons/export.png';
 import logoImg from '/img/logo/atlas_long_white_cut.png';
 import WebInterfaceUtils from '../utils/WebInterfaceUtils';
-import {AtlasNode} from "../utils/AtlasGraph";
-
+import { AtlasNode } from '../utils/AtlasGraph';
 
 export default function Home() {
-    const [libBtnState, setLibBtnState] = useState(
-        `${btnStyles.justBtn} ${btnStyles.libBtn} ${btnStyles.actBtn}`,
-    );
-    const [propBtnState, setPropBtnState] = useState(`${btnStyles.justBtn} ${btnStyles.propBtn}`);
-    const [propsPanelState, setPropsPanelState] = useState(
-        `${styles.propsPanel} ${styles.panelHidden}`,
-    );
-    const [libPanelState, setLibPanelState] = useState(`${styles.libPanel}`);
+	const [libBtnState, setLibBtnState] = useState(
+		`${btnStyles.justBtn} ${btnStyles.libBtn} ${btnStyles.actBtn}`,
+	);
+	const [propBtnState, setPropBtnState] = useState(`${btnStyles.justBtn} ${btnStyles.propBtn}`);
+	const [propsPanelState, setPropsPanelState] = useState(
+		`${styles.propsPanel} ${styles.panelHidden}`,
+	);
+	const [libPanelState, setLibPanelState] = useState(`${styles.libPanel}`);
 
-    const showLibraries = () => {
-        setLibBtnState(`${btnStyles.justBtn} ${btnStyles.libBtn} ${btnStyles.actBtn}`);
-        setPropBtnState(`${btnStyles.justBtn} ${btnStyles.propBtn}`);
-        setPropsPanelState(`${styles.propsPanel} ${styles.panelHidden}`);
-        setLibPanelState(`${styles.libPanel}`);
-    };
-    const showProperties = () => {
-        setPropBtnState(`${btnStyles.justBtn} ${btnStyles.propBtn} ${btnStyles.actBtn}`);
-        setLibBtnState(`${btnStyles.justBtn} ${btnStyles.libBtn}`);
-        setLibPanelState(`${styles.libPanel} ${styles.panelHidden}`);
-        setPropsPanelState(`${styles.propsPanel}`);
-    };
+	const showLibraries = () => {
+		setLibBtnState(`${btnStyles.justBtn} ${btnStyles.libBtn} ${btnStyles.actBtn}`);
+		setPropBtnState(`${btnStyles.justBtn} ${btnStyles.propBtn}`);
+		setPropsPanelState(`${styles.propsPanel} ${styles.panelHidden}`);
+		setLibPanelState(`${styles.libPanel}`);
+	};
+	const showProperties = () => {
+		setPropBtnState(`${btnStyles.justBtn} ${btnStyles.propBtn} ${btnStyles.actBtn}`);
+		setLibBtnState(`${btnStyles.justBtn} ${btnStyles.libBtn}`);
+		setLibPanelState(`${styles.libPanel} ${styles.panelHidden}`);
+		setPropsPanelState(`${styles.propsPanel}`);
+	};
 
+	const [druggedNode, setDruggedNode] = useState<AtlasNode | null>(null);
+	const [selectedNode, setSelectedNode] = useState<AtlasNode | null>(null);
+	const [uiNodes, setUiNodes] = useState(WebInterfaceUtils.getUiNodes(atlasGraph));
+	const [uiEdges, setUiEdges] = useState(WebInterfaceUtils.getUiEdges(atlasGraph));
+	const webInterfaceUtils = new WebInterfaceUtils(
+		atlasGraph,
+		selectedNode,
+		setUiNodes,
+		setUiEdges,
+		setSelectedNode,
+	);
 
-    const [druggedNode, setDruggedNode] = useState<AtlasNode | null>(null);
-    const [selectedNode, setSelectedNode] = useState<AtlasNode | null>(null);
-    const [uiNodes, setUiNodes] = useState(WebInterfaceUtils.getUiNodes(atlasGraph));
-    const [uiEdges, setUiEdges] = useState(WebInterfaceUtils.getUiEdges(atlasGraph));
-    const webInterfaceUtils = new WebInterfaceUtils(
-        atlasGraph,
-        selectedNode,
-        setUiNodes,
-        setUiEdges,
-        setSelectedNode,
-    );
+	useEffect(() => {
+		selectedNode ? showProperties() : showLibraries();
+	}, [selectedNode]);
 
-    useEffect(() => {
-        selectedNode ? showProperties() : showLibraries()
-    }, [selectedNode])
+	return (
+		<>
+			<Head>
+				<title>Atlas Next</title>
+			</Head>
+			<div className={styles.layout}>
+				<div className={styles.leftTop}>
+					<Image
+						src={menuImg}
+						width={'20px'}
+						height={'20px'}
+						layout={'intrinsic'}
+						objectFit={'contain'}
+					/>
+					<Image
+						src={logoImg}
+						alt="Atlas Logo"
+						width={'100%'}
+						height={'100%'}
+						layout={'intrinsic'}
+						objectFit={'contain'}
+					/>
+				</div>
+				<div className={styles.centerTop}>
+					<a>\mockup_name1</a>
+				</div>
+				<div className={styles.rightTop}>
+					<Image
+						src={exportImg}
+						width={'20px'}
+						height={'20px'}
+						layout={'intrinsic'}
+						objectFit={'contain'}
+					/>
+					<Image
+						src={questionImg}
+						width={'10px'}
+						height={'100%'}
+						layout={'intrinsic'}
+						objectFit={'contain'}
+					/>
+					<Image
+						src={settingsImg}
+						width={'20px'}
+						height={'20px'}
+						layout={'intrinsic'}
+						objectFit={'contain'}
+					/>
+				</div>
 
-    return (
-        <>
-            <Head>
-                <title>Atlas Next</title>
-            </Head>
-            <div className={styles.layout}>
-                <div className={styles.leftTop}>
-                    <Image
-                        src={menuImg}
-                        width={'20px'}
-                        height={'20px'}
-                        layout={'intrinsic'}
-                        objectFit={'contain'}
-                    />
-                    <Image
-                        src={logoImg}
-                        alt="Atlas Logo"
-                        width={'100%'}
-                        height={'100%'}
-                        layout={'intrinsic'}
-                        objectFit={'contain'}
-                    />
-                </div>
-                <div className={styles.centerTop}>
-                    <a>\mockup_name1</a>
-                </div>
-                <div className={styles.rightTop}>
-                    <Image
-                        src={exportImg}
-                        width={'20px'}
-                        height={'20px'}
-                        layout={'intrinsic'}
-                        objectFit={'contain'}
-                    />
-                    <Image
-                        src={questionImg}
-                        width={'10px'}
-                        height={'100%'}
-                        layout={'intrinsic'}
-                        objectFit={'contain'}
-                    />
-                    <Image
-                        src={settingsImg}
-                        width={'20px'}
-                        height={'20px'}
-                        layout={'intrinsic'}
-                        objectFit={'contain'}
-                    />
-                </div>
+				{/*------- Buttons*/}
+				<div className={libBtnState} onClick={showLibraries}>
+					<label>Libraries</label>
+				</div>
+				<div className={`${btnStyles.justBtn} ${btnStyles.assetsBtn}`}>
+					<label>Assets</label>
+				</div>
+				<div className={propBtnState} onClick={showProperties}>
+					<label>Properties</label>
+				</div>
+				<div className={`${btnStyles.justBtn} ${btnStyles.objBtn}`}>
+					<label>Object</label>
+				</div>
+				<div className={`${btnStyles.justBtn} ${btnStyles.mockupBtn}`}>
+					<label>Mockup</label>
+				</div>
+				<div className={`${btnStyles.justBtn} ${btnStyles.envBtn}`}>
+					<label>Environment</label>
+				</div>
+				<div
+					className={`${btnStyles.justBtn} ${btnStyles.elementsBtn} ${btnStyles.actBtn}`}
+				>
+					<label>Elements</label>
+				</div>
+				<div className={`${btnStyles.justBtn} ${btnStyles.layersBtn}`}>
+					<label>Layers</label>
+				</div>
 
-                {/*------- Buttons*/}
-                <div className={libBtnState} onClick={showLibraries}>
-                    <label>Libraries</label>
-                </div>
-                <div className={`${btnStyles.justBtn} ${btnStyles.assetsBtn}`}>
-                    <label>Assets</label>
-                </div>
-                <div className={propBtnState} onClick={showProperties}>
-                    <label>Properties</label>
-                </div>
-                <div className={`${btnStyles.justBtn} ${btnStyles.objBtn}`}>
-                    <label>Object</label>
-                </div>
-                <div className={`${btnStyles.justBtn} ${btnStyles.mockupBtn}`}>
-                    <label>Mockup</label>
-                </div>
-                <div className={`${btnStyles.justBtn} ${btnStyles.envBtn}`}>
-                    <label>Environment</label>
-                </div>
-                <div
-                    className={`${btnStyles.justBtn} ${btnStyles.elementsBtn} ${btnStyles.actBtn}`}
-                >
-                    <label>Elements</label>
-                </div>
-                <div className={`${btnStyles.justBtn} ${btnStyles.layersBtn}`}>
-                    <label>Layers</label>
-                </div>
-
-                {/*-----Panels*/}
-                <ElementsPanel visibleState={styles.leftpanel}/>
-                <DnDFlow
-                    webInterfaceUtils={webInterfaceUtils}
-                    druggedNode={druggedNode}
-                />
-                <PropsPanel
-                    propPanelStyleWrapper={propsPanelState}
-                    webInterfaceUtils={webInterfaceUtils}
-                />
-                <LibPanel
-                    setDruggedNode={setDruggedNode}
-                    libPanelStyleWrapper={libPanelState}
-                />
-            </div>
-        </>
-    );
+				{/*-----Panels*/}
+				<ElementsPanel visibleState={styles.leftpanel} />
+				<DnDFlow webInterfaceUtils={webInterfaceUtils} druggedNode={druggedNode} />
+				<PropsPanel
+					propPanelStyleWrapper={propsPanelState}
+					webInterfaceUtils={webInterfaceUtils}
+				/>
+				<LibPanel setDruggedNode={setDruggedNode} libPanelStyleWrapper={libPanelState} />
+			</div>
+		</>
+	);
 }
