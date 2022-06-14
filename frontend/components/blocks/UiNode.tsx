cb6cc440194e1e97e7141d86880fd0b308443b51
Roman Timo <romantimos@gmail.com>
2022-06-14 13:21:34 +0300

    refactoring of code

--- frontend/components/blocks/UiNode.tsx
+++ frontend/components/blocks/UiNode.tsx
@@ -11,18 +11,17 @@ export const uiNodeTypes = {
 };
 
 export function UiBlockWrapper(
-	blockClass: string,
 	name: string,
-	content: JSX.Element,
+	content: JSX.Element | string,
 	result: string | null = null,
-) {
+): JSX.Element {
 	function insertResult(): JSX.Element {
 		if (!result) return <></>;
 		return <div className={styles.result}>{result}</div>;
 	}
 
 	return (
-		<div className={`${styles.block} ${blockClass}`}>
+		<div className={styles.block}>
 			<Handle type="target" position={Position.Left} />
 			<Handle type="source" position={Position.Right} id="a" />
 			<div className={styles.name}>{name}</div>
@@ -33,7 +32,7 @@ export function UiBlockWrapper(
 }
 
 export function TextBlock({ data }: { data: { node: TextNode } }) {
-	return UiBlockWrapper(styles.text_block, data.node.name, <div>{data.node.content}</div>);
+	return UiBlockWrapper(data.node.name, data.node.content);
 }
 
 export function FileBlock({ data }: { data: { node: FileNode } }) {
@@ -42,12 +41,10 @@ export function FileBlock({ data }: { data: { node: FileNode } }) {
 
 	const uploadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (event.target.files === null) return;
-
 		FileUtils.getFileContentString(
 			event.target.files[0],
 			(content: string) => (data.node.content = content),
 		);
-		data.node.filename = event.target.files[0].name;
 		setImportedFileName(event.target.files[0].name);
 	};
 
@@ -56,11 +53,10 @@ export function FileBlock({ data }: { data: { node: FileNode } }) {
 	};
 
 	return UiBlockWrapper(
-		styles.text_block,
 		data.node.name,
 		<>
 			<input className={styles.inputFile} type="file" onChange={uploadFile} />
-			<div style={{ lineHeight: '30px' }}>Imported file: {importedFileName}</div>
+			<div className={styles.importedFiletext}>Imported file: {importedFileName}</div>
 			<label>
 				<input className={styles.inputFile} type="checkbox" onChange={showFileContent} />{' '}
 				Show content
@@ -71,10 +67,5 @@ export function FileBlock({ data }: { data: { node: FileNode } }) {
 }
 
 export function ExpressionBlock({ data }: { data: { node: ExpressionNode } }) {
-	return UiBlockWrapper(
-		styles.expression_block,
-		data.node.name,
-		<div>{data.node.content}</div>,
-		data.node.result,
-	);
+	return UiBlockWrapper(data.node.name, data.node.content, data.node.result);
 }
